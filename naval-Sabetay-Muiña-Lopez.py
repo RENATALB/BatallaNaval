93a2f116f2ec24a2f47f73d0c3d105e6a0c35206
RENATALB <166546709+RENATALB@users.noreply.github.com>
2025-04-10 10:33:48 -0300

    avanzo minimo - comentarios

--- naval-Sabetay-Muiña-Lopez.py
+++ naval-Sabetay-Muiña-Lopez.py
@@ -1,3 +1,16 @@
-CANTIDAD_DISPAROS: int = 10
-CANTIDAD_BARCOS: int = 7 # Puse la varriable en mayuscula para yo entender que es una constante
+import random
 
+tablero = []
+
+CANTIDAD_DISPAROS: int = 10
+CANTIDAD_BARCOS: int = 5 # Puse la varriable en mayuscula para yo entender que es una constante
+"""
+Tablero: 5x5
+Persona vs computadora
+10 disparos
+5 barcos
+"""
+
+
+    x = int(input("X: "))
+    y = int(input("Y: "))
