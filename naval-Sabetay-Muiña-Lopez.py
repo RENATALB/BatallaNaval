2690688a16a776d93a13889746e19bb2fac6456a
RENATALB <166546709+RENATALB@users.noreply.github.com>
2025-04-14 14:03:01 -0300

    generador barcos aleatorio funciona

--- naval-Sabetay-Muiña-Lopez.py
+++ naval-Sabetay-Muiña-Lopez.py
@@ -15,15 +15,16 @@ TAMAÑO_TABLERO = int(input("Tamaño tablero: "))
 #Se pone false ya que no hay nada ahi, es el predeterminado del tablero
 tablero:list = [[False for _ in range(TAMAÑO_TABLERO)] for _ in range(TAMAÑO_TABLERO)]
 
-for fila in (tablero):
-    print(fila)
+#x = int(input("X: ")) #Este input no puede estar suelto, deberia estar en el bucle FOR porque se repite
+#y = int(input("Y: ")) #Este input no puede estar suelto, deberia estar en el bucle FOR porque se repite
 
-x = int(input("X: ")) #Este input no puede estar suelto, deberia estar en el bucle FOR porque se repite
-y = int(input("Y: ")) #Este input no puede estar suelto, deberia estar en el bucle FOR porque se repite
-
-def generadorBarcos(tablero, CANTIDAD_BARCOS):
+def generadorBarcos(tablero, CANTIDAD_BARCOS): # Esta función es para posicionar de manera aleatoria los barcos que se deben adivinar
     barcosPosicionados:int = 0
-    while barcosPosicionados <= CANTIDAD_BARCOS:
+    while barcosPosicionados < CANTIDAD_BARCOS:
         x = random.randint(0, TAMAÑO_TABLERO - 1)
         y = random.randint(0, TAMAÑO_TABLERO - 1)
-        if tablero [x] [y]
+        if not tablero [x] [y]:
+            tablero [x] [y] = True # Si esta casilla del tablero no está ocupada, se pone el barco y se convierte en true
+            barcosPosicionados += 1
+
+generadorBarcos(tablero, CANTIDAD_BARCOS)
